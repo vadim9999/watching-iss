dde9ea349d71725e35ec9089c30efd01af526c8a
Vadim <vadim.putrov@gmail.com>
2024-03-12 19:49:47 +0200

    update all packages

---
+++ src/AppNext.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AppNext = () => {
+  return <div>AppNext</div>;
+};
+
+export default AppNext;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import store from './store/index'
-import { Provider } from "react-redux"
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import AppNext from "./AppNext";
+// import App from "./App";
+// import * as serviceWorker from './serviceWorker';
+// import store from "./store/index";
+// import { Provider } from "react-redux";
 
-ReactDOM.render(<Provider store={store}>
+const container = document.getElementById("app");
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
 
-<App />
-</Provider>
-, document.getElementById('root'));
+root.render(
+  <AppNext />
+  //   <Provider store={store}>
+  //     <App />
+  //   </Provider>
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
