1ad0688a25dec5b8688ce2b3d9899a3ab6568599
Vadim <vadim.putrov@gmail.com>
2024-04-23 19:43:56 +0300

    refactor remove comments

--- src/redux/store/index.ts
+++ src/redux/store/index.ts
@@ -1,25 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
 import stationReducer from "../station/stationSlice";
-// import rootReducer from "../reducers/index";
 import createSagaMiddleware from "redux-saga";
 import mySaga from "../sagas/api-saga";
 
 const sagaMiddleware = createSagaMiddleware();
-// const storeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export const store = configureStore({
   reducer: { station: stationReducer },
-  // middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(sagaMiddleware),
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware().concat(sagaMiddleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
-// const store = createStore(
-//   rootReducer,
-//   storeEnhancers(applyMiddleware(initialiseSagaMiddleware))
-// );
 
 sagaMiddleware.run(mySaga);
-
-// export default store;
